6b577001f74939c403724ea11abcaf566ac614b5
rpiue <enviomagico@gmail.com>
2024-10-29 15:59:05 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Define el puerto en el que se ejecutará el servidor
+const PORT = 3000;
+
+// Usa la carpeta "public" para servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta principal que envía el archivo index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Inicia el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado en http://localhost:${PORT}`);
+});
