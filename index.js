a0d9f34c7ad2848d41a9c2f4ceda9e698b108711
rpiue <enviomagico@gmail.com>
2025-01-03 05:25:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/yape', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'yapeRedirect.html'));
+});
+
+
 // Inicia el servidor
 app.listen(PORT, () => {
   console.log(`Servidor iniciado en http://localhost:${PORT}`);
