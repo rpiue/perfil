b29d19bbf6bf336e7311249cc973119546d09aa1
rpiue <enviomagico@gmail.com>
2025-05-19 03:59:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,66 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+import { getDoc, doc } from 'firebase/firestore/lite';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDCpa3Pg4hcwxrnWl3-Fb4IhqqsDPO1wbg",
+    authDomain: "controller-b0871.firebaseapp.com",
+    projectId: "controller-b0871",
+    storageBucket: "controller-b0871.firebasestorage.app",
+    messagingSenderId: "664100615717",
+    appId: "1:664100615717:web:4837b6cad282940a4031cc",
+    measurementId: "G-H5705PFPCW"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 // Define el puerto en el que se ejecutará el servidor
 const PORT = process.env.PORT || 3000;
 
 // Usa la carpeta "public" para servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.get('/:id', async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const docRef = doc(db, 'links', id);
+    const docSnap = await getDoc(docRef);
+
+    if (!docSnap.exists()) {
+      return res.status(404).send('❌ Enlace no encontrado');
+    }
+
+    const { originalUrl, title, description, image } = docSnap.data();
+
+    res.send(`
+      <!DOCTYPE html>
+      <html lang="es">
+      <head>
+        <meta charset="UTF-8">
+        <meta property="og:title" content="${title}" />
+        <meta property="og:description" content="${description}" />
+        <meta property="og:image" content="${image}" />
+        <meta property="og:url" content="${req.protocol}://${req.get('host')}/${id}" />
+        <meta http-equiv="refresh" content="0; url=${originalUrl}" />
+        <title>Redirigiendo...</title>
+        <script>window.location.href = "${originalUrl}";</script>
+      </head>
+      <body>
+        <h1>Redirigiendo...</h1>
+      </body>
+      </html>
+    `);
+  } catch (err) {
+    console.error('❌ Error al recuperar el link:', err.message);
+    res.status(500).send('Error interno');
+  }
+});
 // Ruta principal que envía el archivo index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
