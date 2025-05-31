bdbaf3acbdb56bb74b1bbe41cc638f77c86398e9
rpiue <enviomagico@gmail.com>
2025-05-31 09:53:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // server.js
 const { initializeApp } = require("firebase/app");
 const { getFirestore, getDoc, doc  } = require("firebase/firestore/lite");
+const { generarLinkPago, ACCESS_TOKEN } = require('./pago');
 
 const express = require('express');
 const path = require('path');
@@ -19,6 +20,11 @@ const firebaseConfig = {
 const appf = initializeApp(firebaseConfig);
 const db = getFirestore(appf);
 
+
+const {
+    darPlan,
+} = require("./db/dato-firebase");
+
 // Define el puerto en el que se ejecutará el servidor
 const PORT = process.env.PORT || 3000;
 
@@ -34,6 +40,86 @@ app.get('/yape', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'yapeRedirect.html'));
 });
 
+app.post('/sara', async (req, res) => {
+    try {
+      const { email, planNombre } = req.body;
+      console.log('Iniciando')
+  
+      if (!email || !planNombre) {
+        return res.status(400).json({ error: 'Faltan campos requeridos: email, monto o planNombre' });
+      }
+
+      let monto
+      if (planNombre.toLowerCase().includes('medium')) {
+        monto = 35;
+      } else if (planNombre.toLowerCase().includes('basico')) {
+        monto = 30;
+      }
+      if(monto > 0){
+      console.log('Creando link')
+        const paymentLink = await generarLinkPago({
+        email,
+        name: email.split('@')[0], // ejemplo simple para generar un nombre
+        monto,
+        descripcion: `${planNombre}`
+      });
+  
+      res.json({ link: paymentLink });
+      }else{
+      res.status(500).json({ error: 'Error al generar el link de pago' });
+
+      }
+      
+    } catch (error) {
+      console.error('Error al generar el link de pago:', error);
+      res.status(500).json({ error: 'Error al generar el link de pago' });
+    }
+  });
+
+
+app.post('/webhook', async (req, res) => {
+    const { type, data } = req.body;
+    if (type === 'payment') {
+
+        const paymentId = data.id;
+
+        try {
+            const payment = await axios.get(`https://api.mercadopago.com/v1/payments/${paymentId}`, {
+                headers: {
+                    Authorization: `Bearer ${ACCESS_TOKEN}`
+                }
+            });
+
+            const pago = payment.data;
+
+            if (pago.status === 'approved') {
+                const emailPagador = pago.payer.email;
+                const userData = planUser.get(emailPagador);
+
+
+
+                if (userData && userData.numero && userData.plan && userData.nombre) {
+                    await darPlan(emailPagador, userData.plan)
+                    // 🔁 Enviar POST a /confirmar con los datos
+                    //await axios.post('https://f4ee-38-224-225-141.ngrok-free.app/confirmar', {
+                    //    nombre: userData.nombre,
+                    //    numero: userData.numero,
+                    //    plan: userData.plan
+                    //});
+
+                    //console.log(`📤 POST a /confirmar enviado para ${emailPagador}`);
+                } else {
+                    //console.log(`⚠️ No se encontró la información para ${emailPagador}`);
+                }
+            }
+
+        } catch (err) {
+            //console.error('❌ Error consultando pago:', err.response?.data || err.message);
+        }
+    }
+
+    res.sendStatus(200);
+});
 
 app.get('/bcp', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'bcpRedirect.html'));
