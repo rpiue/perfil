abc18bbb0cedbf92cf0dda9742c943b0f0f0de10
rpiue <enviomagico@gmail.com>
2025-06-01 22:00:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const { generarLinkPago, ACCESS_TOKEN } = require('./pago');
 
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
+
 const app = express();
 app.use(express.json());
 
